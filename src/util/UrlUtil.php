c3c2ea6435d4346e7b9174c453fa3c1ea29e0883
mnykj <211983651@qq.com>
2024-03-23 09:21:53 +0800

    e

---
+++ src/util/UrlUtil.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * @author 八级天
+ * @since 16:09 2023/12/13
+ * @blog www.mnykj.com
+ * @email 211983651@qq.com
+ */
+
+namespace Mnykj\MnycmsSdkPhp\util;
+
+/**
+ * Class UrlTool
+ * @package Mnykj\MnycmsSdkPhp\tool
+ * url工具类
+ */
+class UrlUtil
+{
+    /**
+     * 获取当前请求的完整域名地址
+     */
+    public static function getEnvUrl(){
+        $protocol = isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? 'https://' : 'http://';
+        $domain = $_SERVER['SERVER_NAME'];
+        return $protocol.$domain."/";
+    }
+}
